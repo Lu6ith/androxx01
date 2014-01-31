d456c52867d5da50e8f5cc27a23c3ae692c7fd55
Lu6ith <wojtlub@gmail.com>
2014-01-31 13:49:28 +0100

    add takePict API function

--- app.js
+++ app.js
@@ -15,6 +15,12 @@ var url = require( "url" );
 
 var app = express();
 
+var util  = require('util'),
+    spawn = require('child_process').spawn;
+    //ls    = spawn('ls', ['-lh', '/usr']);
+    
+//var scribe = require('./scribe');    
+
 // all environments
 app.set('port', process.env.PORT || 8079);
 app.set('views', path.join(__dirname, 'views'));
@@ -26,6 +32,7 @@ app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+//app.use(scribe.logger);
 
 // development only
 if ('development' == app.get('env')) {
@@ -36,6 +43,19 @@ if ('development' == app.get('env')) {
 app.get('/', routes.indexx);
 app.get('/users', user.list);
 
+// You can use any of the following anywhere Scribe will
+// both SAVE to file and PRINT to console whatever
+
+/**
+console.log("[Tagname]Simple message");
+console.info("[Tagname]Simple message");
+console.warn("[Tagname]Simple message");
+console.error("[Tagname]Simple message");
+console.realtime("[Tagname]Simple message");
+console.high("[Tagname]Simple message");
+console.normal("[Tagname]Simple message");
+console.low("[Tagname]Simple message"); */
+
 var APIFunctions = {
  
 		GET : {
@@ -45,10 +65,30 @@ var APIFunctions = {
 				data.url.led = parseInt( data.url.led, 0);
 				if( data.url.value === 1 || data.url.value === 0){
 				
-					console.log( data.url.led, data.url.value );
+					console.info( "[ledSwitch]", data.url.led, data.url.value );
 				}
  
 				callback( data.url.value );
+			},
+			takePict : function (data, callback) {
+			    //var pocv  = spawn('/home/linaro/pubwww/androxx01/public/cgi-bin/pocv.py', []);
+		        var pocv = spawn('/home/linaro/pubwww/androxx01/public/cgi-bin/pocv.py', []);
+	            var outcode;
+			    
+			    pocv.stdout.on('data', function (data) {
+                  console.info('[takePict]stdout: ' + data);
+                });
+
+                pocv.stderr.on('data', function (data) {
+                  console.error('[takePict]stderr: ' + data);
+                });
+
+                pocv.on('exit', function (code) {
+                  outcode = code;
+                  console.log('[takePict]child process exited with code ' + code);
+                  callback(outcode);
+                });
+                
 			}
 		},
 		POST : {}
@@ -70,3 +110,5 @@ narf.setDebug( true );
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server is listening on port ' + app.get('port'));
 });
+
+
